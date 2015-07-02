a0c6a0a41f4aae7b4b8ede4e58cadc9a2a8041fe
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2015-07-02 16:06:02 +0300

    server starts on 0.0.0.0 now (hardcoded),
    infinite listening loop added,
    fixed error with null readLine()
    exit by Ctrl-C added

--- src/main/java/co/socibo/MainApp.java
+++ src/main/java/co/socibo/MainApp.java
@@ -4,8 +4,11 @@ import co.socibo.bot.Bot;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -20,34 +23,48 @@ public class MainApp {
     static final Bot bot = new Bot();
     public static void main(String... args) throws Exception {
 
+	final AtomicBoolean shutdownCalled = new AtomicBoolean(false);
+
 	final ScriptEngineManager manager = new ScriptEngineManager();
 	final ScriptEngine engine = manager.getEngineByName("JavaScript");
 
 	engine.put("bot", bot);
+
+	Runtime.getRuntime().addShutdownHook(new Thread() {
+		public void run() { shutdownCalled.set(true); }
+	    });
 	
-	ServerSocket welcomeSocket = new ServerSocket(6789);
-	final Socket connectionSocket = welcomeSocket.accept();
-	while(true){
-	    String line;
+	ServerSocket serverSock = new ServerSocket();
+	InetAddress addr = InetAddress.getByName("0.0.0.0");
+	serverSock.bind(new InetSocketAddress(addr, 6789));
+
+	while(!shutdownCalled.get()){
+	
+	    final Socket connectionSocket = serverSock.accept();
+	    while(!shutdownCalled.get()){
+		String line;
 	    
-	    BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-	    DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-	    line = inFromClient.readLine();
+		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+		DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+		line = inFromClient.readLine();
+		if(line == null)
+		    break; // end session
 
 	    
-	    try {
-		Object result = engine.eval(line);
-		if(result != null){
-		    outToClient.writeChars(result.toString() + "\n");		    
-		} else {
-		    outToClient.writeChars("OK\n");
-		};
+		try {
+		    Object result = engine.eval(line);
+		    if(result != null){
+			outToClient.writeChars(result.toString() + "\n");		    
+		    } else {
+			outToClient.writeChars("OK\n");
+		    };
 
 
-	    } catch (Exception e) {
-		e.printStackTrace();
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
 	    }
-	} 
+	}
     }
 }
 
